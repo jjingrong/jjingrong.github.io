9959a8c4c2890fd30498fc26dd166c4f3de870f1
jing rong <jing_rong92@hotmail.com>
2022-06-10 23:51:43 +0800

    Footer

---
+++ src/Links.ts
@@ -0,0 +1,6 @@
+const Links = {
+    GITHUB: 'https://github.com/jjingrong',
+    LINKEDIN: 'https://www.linkedin.com/in/limjingrong/'
+};
+
+export default Links
--- src/components/AppFooter/index.tsx
+++ src/components/AppFooter/index.tsx
@@ -1,14 +1,30 @@
 import './index.css';
 
-import { Container, Typography } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { Container, Grid, IconButton } from '@mui/material';
+import { useMemo } from 'react';
+
+import Links from '../../Links';
 
 const AppFooter = () => {
+  const LinkedIn = () => (
+    <IconButton href={Links.LINKEDIN} target="_blank">
+      <LinkedInIcon fontSize={'large'} />
+    </IconButton>
+  );
+
+  const Github = () => (
+    <IconButton href={Links.GITHUB} target="_blank">
+      <GitHubIcon fontSize={'large'} />
+    </IconButton>
+  );
+
   return (
     <div className="App-footer">
       <Container maxWidth="lg">
-        <Typography variant="h1" component="h1">
-          Footer - placeholder
-        </Typography>
+        <Github />
+        <LinkedIn />
       </Container>
     </div>
   );
